e4ce0f146ff680ece3437dd5b930792194342937
nils-matthaei <n.matthaei@proton.me>
2025-06-17 13:19:18 +0200

    Replace set with stack for the example

---
+++ src/stack.go
@@ -0,0 +1,40 @@
+package main
+
+type Stack[T any] struct {
+	data []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{}
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.data = append(s.data, value)
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(s.data) == 0 {
+		var zero T
+		return zero, false
+	}
+	index := len(s.data) - 1
+	val := s.data[index]
+	s.data = s.data[:index]
+	return val, true
+}
+
+func (s *Stack[T]) Peek() (T, bool) {
+	if len(s.data) == 0 {
+		var zero T
+		return zero, false
+	}
+	return s.data[len(s.data)-1], true
+}
+
+func (s *Stack[T]) Len() int {
+	return len(s.data)
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.data) == 0
+}
